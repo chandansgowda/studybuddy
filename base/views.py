34edb8301d108d240a73637f9e8f7378994afd62
chandansgowda <chandansuresh007@gmail.com>
2022-11-08 20:45:57 +0530

    templates

--- base/views.py
+++ base/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'main.html')
+    return render(request,'base/home.html')
+
+def room(request,pk):
+    return render(request,'base/room.html')
