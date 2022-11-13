f783a24536692115a4b1380800e146fc305808bc
chandansgowda <chandansuresh007@gmail.com>
2022-11-13 14:58:01 +0530

    CRUD and Search

--- base/views.py
+++ base/views.py
@@ -1,12 +1,21 @@
 from django.http import HttpResponse
+from django.db.models import Q
 from django.shortcuts import redirect, render
-from .models import Room
+from .models import Room, Tag
 from .forms import RoomForm
 
 # Create your views here.
 def home(request):
-    rooms = Room.objects.all()
-    return render(request,'base/home.html', {'rooms':rooms})
+    q = request.GET.get('q') if request.GET.get('q')!= None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__contains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+        
+    topics = Tag.objects.all()
+    return render(request,'base/home.html', {'rooms':rooms, 'topics':topics})
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
@@ -26,5 +35,18 @@ def createRoom(request):
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    if request.method =="POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
     context={'form':form}
     return render(request,'base/room_form.html',context)
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    if request.method=="POST":
+        room.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':room})
